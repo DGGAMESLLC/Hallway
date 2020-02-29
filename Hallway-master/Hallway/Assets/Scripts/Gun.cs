7809c60603381cadc0835dccafb6690ae3a97e5e
jehouha <jehouha@knights.ucf.edu>
2020-02-29 05:27:08 -0500

    v0.4
    added player detection raycast (previous version on googledrive)

---
+++ Hallway-master/Hallway/Assets/Scripts/Gun.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 100f;
+    public float fireRate;
+    private float nextFire;
+
+    public Camera fpsCam;
+
+    private AudioManager audioManager;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1") && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            Shoot();
+        }
+
+        void Shoot()
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+            {
+                Gravity_One target = hit.transform.GetComponent<Gravity_One>();
+                if (target != null)
+                {
+                    target.TakeDamage(damage);
+                }
+            }
+        }
+    }
+}
---
+++ Hallway-master/Hallway/Assets/Scripts/SpawnProjectile.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnProjectile : MonoBehaviour
+{
+    public GameObject firePoint;
+    public List<GameObject> vfx = new List<GameObject>();
+    public float fireRate;
+    public float range = 10f;
+
+    private GameObject effectToSpawn;
+    private float nextFire;
+
+    private AudioManager audioManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        effectToSpawn = vfx[0];
+        audioManager = AudioManager.instance;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            SpawnVFX();
+        }
+    }
+
+    void SpawnVFX()
+    {
+        GameObject vfx;
+        audioManager.PlaySound("fireSound");
+        if (firePoint != null)
+        {
+            vfx = Instantiate(effectToSpawn, firePoint.transform.position,
+                firePoint.transform.rotation);
+        }
+        else
+        {
+            Debug.Log("No Fire Point");
+        }
+    }
+}
